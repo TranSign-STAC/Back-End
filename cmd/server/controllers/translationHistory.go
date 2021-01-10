37de447bfa1d33ab2314573e7bf163b0122da222
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-01-11 05:54:44 +0900

    Add cmd/server/controllers/translationHistory.go for seperating controllers

---
+++ cmd/server/controllers/translationHistory.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"context"
+	"transign/cmd/server/config"
+	"transign/cmd/server/models"
+	pb "transign/gen"
+)
+
+type TranslationHistoryServer struct {
+	pb.UnimplementedTranslationHistoryServer
+}
+
+// GetHistory is a function for handling retrieving list of history request
+func (s *TranslationHistoryServer) GetHistory(ctx context.Context, in *pb.UUIDMessage) (*pb.TranslationHistoryResponse, error) {
+	var translations []models.Translation
+	var requestHistory []*pb.TextToSignLangRequest
+	config.DB.Where(&models.Translation{UUID: in.Uuid}).Find(&translations)
+	requestHistory = make([]*pb.TextToSignLangRequest, len(translations))
+
+	for i, translation := range translations {
+		requestHistory[i] = &pb.TextToSignLangRequest{Uuid: translation.UUID, Text: translation.Text}
+	}
+
+	response := &pb.TranslationHistoryResponse{History: requestHistory}
+	return response, nil
+}
